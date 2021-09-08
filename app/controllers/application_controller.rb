6adceeebe37627e23deea7f07a7f3a11208ec721
Ana Cury <apaulacury@gmail.com>
2021-09-08 17:10:35 -0300

    little repairs on the login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,4 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :cpf, :phone_number, :photo])
   end
-  def after_sign_in_path_for(resource)
-    maps_path
-  end
 end
